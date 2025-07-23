e4174bf44bf2f28538ffa61a2a77de6ea44b78ef
Harsh Pathak <103025942+ClasherFTW@users.noreply.github.com>
2025-07-23 09:32:30 +0530

    Update employee_salary.cpp

--- day1/employee_salary.cpp
+++ day1/employee_salary.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<string>    
+using namespace std;
+int main(){
+    int id,salary;
+    string name;    
+    int exp;
+    int bonus = 0;
+    cout << "Enter Employee ID: ";
+    cin >> id;  
+    cout << "Enter Employee Name: ";
+    cin.ignore(); 
+    getline(cin, name);
+    cout << "Enter Employee Salary: ";
+    cin >> salary;  
+    cout << "Enter Employee Experience (in years): ";
+    cin >> exp; 
+    if(exp<2 ) {
+        bonus = salary * 0.05; 
+    } else if(exp >= 2 && exp < 5) {
+        bonus = salary * 0.1; 
+    } else (exp >= 5 ); {
+        bonus = salary * 0.15;
+    } 
+    cout << "salary is " << salary + bonus << endl;
+    return 0;
 
+}
